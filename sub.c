60bbedaa2018f6698c6b4d2a65e887f518af9945
sunandini22 <skadiyala@umassd.edu>
2025-12-02 20:52:14 -0600

    added sub.c

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 5;
+    int result_subtraction;
+
+    result_subtraction = num1 - num2; // Subtracting num2 from num1
+
+    printf("The result of subtraction is: %d\n", result_subtraction); // Output: 5
+
+    return 0;
+}
